0484f92d6600abdf4613cc03a6beb918cbf7d2c3
Pavel Ivanov <ivanov@hp.nct>
2016-07-27 12:12:23 +0300

    Config seems to be done

--- 20_cgroup.pp
+++ 20_cgroup.pp
@@ -2,9 +2,8 @@ file {
     '/etc/sysconfig':     ensure  => directory;
     '/etc/cgconfig.conf': content => template('base/cgconfig.conf.erb');
     '/etc/cgrules.conf':  content => "$lxc_user * $lxc_user";
-    '/lib/systemd/system/cgconfig.service':
-        mode    => '0755',
-        content => template('base/cgconfig.service');
+    '/lib/systemd/system/cgconfig.service': content => template('base/cgconfig.service');
+    '/lib/systemd/system/cgred.service':    content => template('base/cgred.service');
 } -> 
 exec {
     'cp-cgred':
@@ -12,4 +11,5 @@ exec {
         command => 'cp /usr/share/doc/cgroup-tools/examples/cgred.conf /etc/sysconfig/cgred.conf',
 	creates => '/etc/sysconfig/cgred.conf';
 } ->
-service { 'cgconfig':; }
+service { ['cgconfig', 'cgred']:; }
+
